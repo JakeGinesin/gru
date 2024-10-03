d63e04144c6dfdc798c21a25f05405290786034e
JakeGinesin <wendellthenerd123@gmail.com>
2024-10-03 14:39:43 -0400

    fixes

--- setup.py
+++ setup.py
@@ -25,11 +25,12 @@ setup(
         'console_scripts': [
             #'tighten-pbt=gru.refine_pbts:tighten_repo_pbt',
             #'generalize-pbt=gru.refine_pbts:generalize_repo_pbt',
-            'tighten-pbt=gru.refine_pbts:main',
             'find-pbts=gru.refine_pbts:main',
             'generalize-pbt=gru.refine_pbts:main',
             #'analyze-pbts=gru.analyze_pbts:analyze_pbts_in_repo',
             'analyze-pbts=gru.analyze_pbts:main',
+            'analyze-pbt=gru.analyze_pbts:main',
+            'tighten-pbt=gru.refine_pbts:main',
         ],
     },
     classifiers=[
